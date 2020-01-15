e5c000a20fa4028fa3c4d377672817781fdbfcc3
Yusuf Celik <yusufcelik@icloud.com>
2020-01-15 11:03:10 +0100

    Removed npm license-check on account of stability issues. Added maven license checker and updated license headers.

--- src/hooks/useDataApi.test.tsx
+++ src/hooks/useDataApi.test.tsx
@@ -1,96 +1,98 @@
-// Copyright 2020 Rabobank Nederland
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Copyright (C) 2019 - 2020 Rabobank Nederland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import {act, renderHook} from '@testing-library/react-hooks';
-import axios from 'axios';
+import { act, renderHook } from "@testing-library/react-hooks";
+import axios from "axios";
 
-import MockAdapter from 'axios-mock-adapter';
+import MockAdapter from "axios-mock-adapter";
 
-import IState from '../interfaces/IState';
-import Action from '../types/Action';
-import DataRequest from '../types/DataRequest';
-import useDataApi from './useDataApi';
+import IState from "../interfaces/IState";
+import Action from "../types/Action";
+import DataRequest from "../types/DataRequest";
+import useDataApi from "./useDataApi";
 
 const dataFetchReducer = (state: IState, action: Action) => {
   switch (action.type) {
-    case 'FETCH_INIT':
+    case "FETCH_INIT":
       return {
         ...state,
-        isLoading: true,
+        isLoading: true
       };
-    case 'FETCH_SUCCESS':
+    case "FETCH_SUCCESS":
       return {
         ...state,
         data: action.results,
-        isLoading: false,
+        isLoading: false
       };
-    case 'FETCH_FAILURE':
+    case "FETCH_FAILURE":
       return {
         ...state,
         error: action.error,
-        isLoading: false,
+        isLoading: false
       };
   }
 };
 
 const mock = new MockAdapter(axios);
-const mockUrl = 'http://mock';
+const mockUrl = "http://mock";
 
-describe('useDataApi hook', () => {
- it('fetches data on mount when url is specified at hook initialization', async () => {
+describe("useDataApi hook", () => {
+  it("fetches data on mount when url is specified at hook initialization", async () => {
     const dataRequest: DataRequest = {
-      method: 'get',
-      token: 'my_token',
-      url: mockUrl,
+      method: "get",
+      token: "my_token",
+      url: mockUrl
     };
 
     mock.onGet(mockUrl).reply(200, {
-      message: 'Hello World',
+      message: "Hello World"
     });
 
-    const {result, waitForNextUpdate} = renderHook(() =>
-      useDataApi(dataFetchReducer, dataRequest),
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useDataApi(dataFetchReducer, dataRequest)
     );
 
     await waitForNextUpdate();
 
     expect(result.current[0].data).toEqual({
-      message: 'Hello World',
+      message: "Hello World"
     });
   });
 
-  it('fetch data when no url is specified at the time of hook initialization', () => {
-    const {result} = renderHook(() => useDataApi(dataFetchReducer));
+  it("fetch data when no url is specified at the time of hook initialization", () => {
+    const { result } = renderHook(() => useDataApi(dataFetchReducer));
 
     expect(result.current[0]).toEqual({
-      isLoading: false,
+      isLoading: false
     });
   });
 
-  it('fetches data when new url is set', async () => {
+  it("fetches data when new url is set", async () => {
     const dataRequest: DataRequest = {
-      method: 'get',
-      token: 'my_token',
-      url: mockUrl,
+      method: "get",
+      token: "my_token",
+      url: mockUrl
     };
 
     mock.onGet(mockUrl).reply(200, {
-      message: 'Hello World',
+      message: "Hello World"
     });
 
-    const {result, waitForNextUpdate} = renderHook(() =>
-      useDataApi(dataFetchReducer),
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useDataApi(dataFetchReducer)
     );
 
     act(() => {
@@ -99,24 +101,24 @@ describe('useDataApi hook', () => {
     await waitForNextUpdate();
 
     expect(result.current[0].data).toEqual({
-      message: 'Hello World',
+      message: "Hello World"
     });
   });
 
-  it('posts data when post method is specified', async () => {
+  it("posts data when post method is specified", async () => {
     const dataRequest: DataRequest = {
-      data: {id: 1},
-      method: 'post',
-      token: 'my_token',
-      url: mockUrl,
+      data: { id: 1 },
+      method: "post",
+      token: "my_token",
+      url: mockUrl
     };
 
     mock.onPost(mockUrl).reply(200, {
-      success: true,
+      success: true
     });
 
-    const {result, waitForNextUpdate} = renderHook(() =>
-      useDataApi(dataFetchReducer, dataRequest),
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useDataApi(dataFetchReducer, dataRequest)
     );
 
     act(() => {
@@ -126,7 +128,7 @@ describe('useDataApi hook', () => {
     await waitForNextUpdate();
 
     expect(result.current[0].data).toEqual({
-      success: true,
+      success: true
     });
   });
 });
--- src/organisms/DashboardNavbar.tsx
+++ src/organisms/DashboardNavbar.tsx
@@ -1,21 +1,23 @@
-// Copyright 2020 Rabobank Nederland
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Copyright (C) 2019 - 2020 Rabobank Nederland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import ContextMenu from '../molecules/ContextMenu';
-import Navbar from '../molecules/Navbar';
+import React, { useState } from "react";
+import styled from "styled-components";
+import ContextMenu from "../molecules/ContextMenu";
+import Navbar from "../molecules/Navbar";
 
 const AvatarContainer = styled.li`
   position: relative;
@@ -26,7 +28,7 @@ const Avatar = styled.div`
   border: 1px solid ${props => props.theme.dashboardPage.avatar.borderColor};
   height: 2rem;
   width: 2rem;
-  background: url('images/user.svg');
+  background: url("images/user.svg");
   padding: 0.2rem;
   box-sizing: border-box;
 `;
@@ -43,7 +45,7 @@ const TransparentOverlay = styled.div`
 const DashboardNavbar = () => {
   const [displayContextMenu, setDisplayContextMenu] = useState(false);
   const enableContextMenuDisplay = () => {
-     setDisplayContextMenu(true);
+    setDisplayContextMenu(true);
   };
 
   const disableContextMenuDisplay = () => {
