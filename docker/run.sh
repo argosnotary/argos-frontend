00747f0201b2d634e64cf65cbf67d81f78da9204
Gerard Borst <gerard.borst@argosnotary.com>
2020-11-22 22:30:44 +0100

    change license headers

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2020 Argos Notary Coöperatie UA
+# Copyright (C) 2020 Argos Notary
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
